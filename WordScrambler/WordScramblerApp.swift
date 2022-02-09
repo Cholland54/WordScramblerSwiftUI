303ac2b5d4339f4e55297388a318f421a67be6c3
Connor Holland <66492556+ConnorHolland54@users.noreply.github.com>
2022-02-08 17:07:08 -0700

    Setup

---
+++ WordScrambler/WordScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramblerApp.swift
+//  WordScrambler
+//
+//  Created by Connor Holland on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
